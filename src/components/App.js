0ec4dd91e1881cfa1d4f6913158e3dad75658fbf
Simon <sisihamilton@hotmail.com>
2019-07-13 16:48:11 -0400

    made app a class and used callback to send search term from search bar to parent App

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import SearchBar from './search/SearchBar';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )};
+class App extends React.Component {
+
+  onSearchSubmit = (term) => {
+    console.log("Searching", term)
+
+    //TODO send API request to get images
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSearch={ this.onSearchSubmit }/>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -8,25 +8,23 @@ class SearchBar extends React.Component {
   // TODO: Get user location and pre-populate search bar with user city
   state = { term: '' };
 
-  onInputChange(e) {
-    console.log(e.target.value)
-
+  onInputChange = (e) => {
     let term = e.target.value;
     this.setState({term});
   }
 
-  onFormSubmit(e) {
+  onFormSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state.term);
+    this.props.onSearch(this.state.term);
   }
 
   render() {
     return (
-      <form onSubmit={ this.onFormSubmit.bind(this) } className="search_bar_form">
+      <form onSubmit={ this.onFormSubmit } className="search_bar_form">
         <input 
           type="text" 
           value={ this.state.term } 
-          onChange={ this.onInputChange.bind(this) } 
+          onChange={ this.onInputChange } 
         />
       </form>
     )
