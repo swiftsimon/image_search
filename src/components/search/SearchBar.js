7caf798df67cce6b6eac892bd6e67f485042f60e
Simon <sisihamilton@hotmail.com>
2019-07-12 21:46:08 -0400

    made search input a controlled element

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -5,15 +5,29 @@ import "./SearchBar.css";
 
 
 class SearchBar extends React.Component {
+  // TODO: Get user location and pre-populate search bar with user city
+  state = { term: '' };
+
   onInputChange(e) {
     console.log(e.target.value)
+
+    let term = e.target.value;
+    this.setState({term});
   }
 
+  onFormSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.term);
+  }
 
   render() {
     return (
-      <form className="search_bar_form">
-        <input type="text" onChange={this.onInputChange} />
+      <form onSubmit={ this.onFormSubmit.bind(this) } className="search_bar_form">
+        <input 
+          type="text" 
+          value={ this.state.term } 
+          onChange={ this.onInputChange.bind(this) } 
+        />
       </form>
     )
   }
